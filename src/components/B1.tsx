2f448e346ec501ec622da17d62935ea0bb0efdb2
hoang261105 <147359595+hoang261105@users.noreply.github.com>
2024-06-05 11:18:26 +0700

    Nop bai ss53

---
+++ src/components/B1.tsx
@@ -0,0 +1,12 @@
+export default function B1() {
+  function Calculate(a: number, b: number, callback: any) {
+    const result = a + b;
+    callback(result);
+  }
+
+  function Answer(result: number) {
+    console.log(result);
+  }
+  Calculate(10, 20, Answer);
+  return <div></div>;
+}
